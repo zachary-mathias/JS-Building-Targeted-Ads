6a2fad9f1f2478d8e677e0472a5e017d6f262f23
zachary-mathias@hotmail.com <zachary-mathias@hotmail.com>
2023-03-28 21:23:36 -0400

    added some functions for getting coordinates and timezones.

--- public/app.js
+++ public/app.js
@@ -18,3 +18,43 @@
 
 // event listeners
 // on load, build ads
+function userTime(){
+  return new Date().getHours();
+}
+
+function getMealTime() {
+  const tod = userTime()
+  return tod > 20 ? 'late night snack' :  tod > 16 ? 'dinner' : tod > 11 ? 'lunch' : 'breakfast'
+}
+console.log(getMealTime())
+
+function buildAd1() {
+  const mealTime = getMealTime()
+  const div = document.querySelector('.ad1')
+  const p = document.createElement('p')
+  p.innerHTML = `We've got the best <span>${mealTime}</span> in town.`
+  div.append(p)
+}
+
+function buildAd2(coords) {
+  const href = `https://www.google.com/maps/search/coffee/@${coords[0]},${coords[1]},15z`
+  const div = document.querySelector('.ad2')
+  const p = document.createElement('p')
+  p.innerHTML = `Coffee! <span> <a href = ${href}
+  target='blank'>We're this close!</a> </span>`
+  div.append(p)
+}
+
+async function getCoords() {
+  const position = await new Promise((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition(resolve, reject)
+  })
+  return position
+}
+
+
+window.onload = async () => {
+  buildAd1()
+  const coords = await getCoords()
+  buildAd2(coords)
+}
